76f9fd52287fed2896eaa3088acbeb461c2b9401
flareze <89435533+BaskaraPR@users.noreply.github.com>
2023-09-05 07:07:31 +0700

    Add files via upload

---
+++ jobApp/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head><title>FindJob</title></head>
+<body>
+    <h1>Jobless??</h1>
+    <hr>
+    <h2>avalaible jobs</h2>
+    <ul>
+        <li>Make Envelope</li>
+        <li>Translator</li>
+        <li>Dishwasher</li>
+    </ul>
+</body>
