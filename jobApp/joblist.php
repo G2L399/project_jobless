9b456c99f5539d087cc50a1f9d546e2252c98b79
flareze <89435533+BaskaraPR@users.noreply.github.com>
2023-09-05 13:30:00 +0700

    Add files via upload

---
+++ jobApp/joblist.php
@@ -0,0 +1,13 @@
+<?php
+    echo "aaaaaa";
+?>
+
+<!DOCTYPE html>
+<body>
+<h2>avalaible jobs</h2>
+    <ul>
+        <li>Make Envelope</li>
+        <li>Translator</li>
+        <li>Dishwasher</li>
+    </ul>
+</body>
